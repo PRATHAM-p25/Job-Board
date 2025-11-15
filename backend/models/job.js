8187997d9c63a486a0d34a95fc138d9ac06fc087
PRATHAM-p25 <148321716+PRATHAM-p25@users.noreply.github.com>
2025-11-15 20:39:34 +0530

    Update job.js

--- backend/models/job.js
+++ backend/models/job.js
@@ -1,18 +1,16 @@
-// models/job.js
 import mongoose from 'mongoose';
 
 const JobSchema = new mongoose.Schema({
   title: { type: String, required: true, trim: true },
   description: { type: String, required: true },
   skills: { type: [String], required: true },
-  postedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'Admin', default: null }, // NEW
+  postedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'Admin', default: null }, 
   createdAt: { type: Date, default: Date.now }
 }, {
   toJSON: { virtuals: true },
   toObject: { virtuals: true }
 });
 
-// virtual applications count (optional)
 JobSchema.virtual('applications', {
   ref: 'Application',
   localField: '_id',
